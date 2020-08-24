200eb3a1b3fa023e6553b610b2dc3ab68365379f
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-24 20:44:56 +0300

    Initial Commit

---
+++ 3DCardsScrollingAnimationInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  3DCardsScrollingAnimationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 3DCardsScrollingAnimationInSwiftUI/_DCardsScrollingAnimationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DCardsScrollingAnimationInSwiftUIApp.swift
+//  3DCardsScrollingAnimationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 24.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct _DCardsScrollingAnimationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
